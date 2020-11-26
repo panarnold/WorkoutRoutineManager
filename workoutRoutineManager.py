fe747456a74058a85fbb7244677cb03eb0173f80
Arnold Cytrowski <arnold@pop-os.localdomain>
2020-11-26 11:44:15 +0100

    quick #TODO list

---
+++ workoutRoutineManager.py
@@ -0,0 +1,7 @@
+#! python
+# Steps TODO:
+# 1. Create dictionaries of users. Program will collect data about them (weight, age, sex, lifting records etc.)
+# 2. Quick survey to every user - times a week of training, weakspots, prorities muscles
+# 3. Proposition of workout prints on a termianl (random?). If you accept it, just click yes.
+# 4. Script will save it onto .xlsx file
+# 5. Option for writing a .pdf file directly from .xlsx file
